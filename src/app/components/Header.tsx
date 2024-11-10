f9d4d2ccf595cd66151cd60525565c30ccdf0c6a
qwazertox <qwazertyx84@gmail.com>
2024-11-10 18:26:13 +0100

    google auth added

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,14 +1,36 @@
 import Link from "next/link";
+import {auth, signIn, signOut} from "@/auth";
 
-export default function Header() {
+export default async function Header() {
+	const session = await auth();
+
 	return (
 		<header>
 			<div className="container flex items-center justify-between mx-auto my-4">
 
 			<Link href={"/"} className="font-bold text-xl" >VocabCards</Link>
-			<nav className="flex gap-2  *:py-2 *:px-4 *:rounded-md">
-				<Link className= "bg-gray-200" href={"/login"}>Login</Link>
-				<Link className="bg-blue-600 text-white" href={"/new-deck"}>Create a Deck</Link>
+			<nav className="flex gap-2">
+			
+			{!session && (
+				<form action={async () => {
+					'use server';
+					await signIn('google');
+				}}>
+				<button type="submit" className= "bg-gray-200 py-2 px-4 rounded-md">Login with Google</button>
+				</form>
+
+			)}
+			{session && (
+				<form action={async () => {
+					'use server';
+					await signOut();
+				}}>
+				<button type="submit" className= "bg-gray-200 py-2 px-4 rounded-md">Logout</button>
+				</form>
+
+			)}
+
+				<Link className="bg-blue-600 py-2 px-4 rounded-md text-white" href={"/new-deck"}>Create a Deck</Link>
 			</nav>
 			</div>
 		</header>
